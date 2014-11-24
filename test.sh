7f440814ba949d1a7c8855fc1f3a1598789e7376
Stanley <stanley.yang.sy@gmail.com>
2014-11-24 02:57:46 -0500

    now outputs a newline after every sample run

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 javac huffman/Decode.java
 # Run against all samples provided.
-for i in {0..9..1}
+for i in {0..9}
 do
   echo "Sample $i: Differences"
   java huffman.Decode samples/encoded/sample$i.huf test.txt
   diff test.txt samples/text/sample$i.txt
+  echo ""
 done
