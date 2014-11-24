8902d9e08575907cb9acc835b66dc6fda5f2dc3a
Stanley <stanley.yang.sy@gmail.com>
2014-11-24 15:58:46 -0500

    made output more clear

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ javac huffman/Decode.java
 # Run against all samples provided.
 for i in {0..9}
 do
-  echo "Sample $i: Differences"
+  echo "Running Sample $i, Differences:"
   java huffman.Decode samples/encoded/sample$i.huf test.txt
   diff test.txt samples/text/sample$i.txt
   echo ""
